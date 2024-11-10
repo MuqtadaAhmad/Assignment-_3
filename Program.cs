560f260f03f334aa270c84be202d9b75114ef337
MuqtadaAhmad <s1023075@uomustansiriyah.edu.iq>
2024-11-10 20:34:23 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,32 @@ namespace Assignment__3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("how mane student you want enter");
+            int numberStu = Convert.ToInt32(Console.ReadLine());
+            List<Student> st = new List<Student>();
+            for (int i=0;i<numberStu;i++)
+            {
+                Console.WriteLine("The name of student? ");
+                string Name = Console.ReadLine();
+                Console.WriteLine("The id of student? ");
+                string Id = Console.ReadLine();
+                Console.WriteLine("The mark of student?");
+                int Mark = Convert.ToInt32(Console.ReadLine());
+
+                st.Add(new Student { id = Id, mark =Mark, name = Name });
+            }
+            Console.WriteLine("The name of student who passed");
+            for (int i=0;i<st.Count;i++)
+            {
+                if (st[i].mark >= 50)
+                {
+
+                    Console.WriteLine(i+"- "+st[i].name);
+                }
+            
+            }
+          
+            Console.ReadLine();
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment__3
+{
+    public class Student
+    {
+      
+        public string id;
+       public string name;
+        public double mark;
+
+      
+    }
+}
